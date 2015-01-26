fbf55c41ad3f388ae3e50d1f1ea1835631d17919
xinli_xiao <xiaoxinlixxl@gmail.com>
2015-01-25 20:55:35 -0600

    Initial commit

---
+++ src/test2.java
@@ -0,0 +1,14 @@
+/**
+ * Created by xiao on 1/25/2015.
+ */
+public class test2 {
+    int r;
+    String s;
+    String[] c;
+    double[] vector;
+
+    int jwj(){
+        System.out.println("sdfsafds");
+        return 1;
+    }
+}
---
+++ src/test3.java
@@ -0,0 +1,13 @@
+/**
+ * Created by xiao on 1/25/2015.
+ */
+public class test3 {
+    test2 x=new test2();
+
+    public static void main(String[] args) {
+        this.x.s="sdfsafdds";
+        System.out.println(this.x.s);
+
+    }
+
+}
